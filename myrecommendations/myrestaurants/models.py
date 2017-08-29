8c3781ce2d86e2dba98e38dbb45ecb2a0b6eda70
Derek Pham <derekmpham@gmail.com>
2017-08-28 22:45:35 -0700

    Add restaurant review class to model

--- myrecommendations/myrestaurants/models.py
+++ myrecommendations/myrestaurants/models.py
@@ -35,4 +35,15 @@ class Dish(models.Model):
 	def __unicode__(self):
 		return u"%s" % self.name
 
+class Review(models.Model):
+	RATING_CHOICES = ((1, 'one'), (2, 'two'), (3, 'three'), (4, 'four'), (5, 'five'))
+	rating = models.PositiveSmallIntegerField('Rating (stars)', blank=False, default=3, choices=RATING_CHOICES)
+	comment = models.TextField(blank=True, null=True)
+	user = models.ForeignKey(User, default=1)
+	date = models.DateField(default=date.today)
 
+	class Meta:
+		abstract = True
+
+class RestaurantReview(Review):
+	restaurant = models.ForeignKey(Restaurant)
