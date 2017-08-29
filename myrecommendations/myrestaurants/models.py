b336b9240786cfb318a0c11c4242df4970fc041e
Derek Pham <derekmpham@gmail.com>
2017-08-28 22:40:13 -0700

    Add dish class to model

--- myrecommendations/myrestaurants/models.py
+++ myrecommendations/myrestaurants/models.py
@@ -19,3 +19,20 @@ class Restaurant(models.Model):
 	url = models.URLField(blank=True, null=True)
 	user = models.ForeignKey(User, default=1)
 	date = models.DateField(default=date.today)
+
+	def __unicode__(self):
+		return u"%s" % self.name
+
+class Dish(models.Model):
+	name = models.TextField()
+	description = models.TextField(blank=True, null=True)
+	price = models.DecimalField('Euro amount', max_digits=8, decimal_places=2, blank=True, null=True)
+	user = models.ForeignKey(User, default=1)
+	date = models.DateField(default=date.today)
+	image = models.ImageField(upload_to="myrestaurants", blank=True, null=True)
+	restaurant = models.ForeignKey(Restaurant, null=True, related_name='dishes')
+
+	def __unicode__(self):
+		return u"%s" % self.name
+
+
