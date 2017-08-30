9ded35120145fe6277d2d42c2ffcde88d07826f8
Derek Pham <derekmpham@gmail.com>
2017-08-29 22:43:47 -0700

    Define restaurant detail class view

--- myrecommendations/myrestaurants/views.py
+++ myrecommendations/myrestaurants/views.py
@@ -3,4 +3,41 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
+from models import RestaurantReview, Restaurant, Dish
+from forms import RestaurantForm, DishForm
+
 # Create your views here.
+class RestaurantDetail(DetailView):
+	model = Restaurant
+	template_name = 'myrestaurants/restaurant_detail.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(RestaurantDetail, self).get_context_data(**kwargs)
+		context['RATING_CHOICES'] = RestaurantReview.RATING_CHOICES
+		return context
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
