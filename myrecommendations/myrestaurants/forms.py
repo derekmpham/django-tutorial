dc7a8ccdb7bb9ed05bbe2096a527cbb55ef9448a
Derek Pham <derekmpham@gmail.com>
2017-09-02 23:32:55 -0700

    Debug for schema migration

--- myrecommendations/myrestaurants/forms.py
+++ myrecommendations/myrestaurants/forms.py
@@ -4,10 +4,9 @@ from models import Restaurant, Dish
 class RestaurantForm(ModelForm):
 	class Meta:
 		model = Restaurant
-		exclude ('user', 'date',)
+		exclude = ('user', 'date',)
 
 class DishForm(ModelForm):
 	class Meta:
 		model = Dish
 		exclude = ('user', 'date', 'restaurant',)
-		
--- myrecommendations/myrestaurants/urls.py
+++ myrecommendations/myrestaurants/urls.py
@@ -4,58 +4,59 @@ from django.views.generic import DetailView, ListView, UpdateView
 from models import Restaurant, Dish
 from forms import RestaurantForm, DishForm
 from views import RestaurantCreate, DishCreate, RestaurantDetail
+from myrestaurants.views import review
 
 urlpatterns = [
 
 # List latest 5 restaurants: /myrestaurants/
-	url(r'\^\$',
-		ListView.as_view(
-			queryset=Restaurant.objects.filter(date_lte=timezone.now()).order_by('date')[:5],
-			context_object_name='latest_restaurant_list',
-			template_name='myrestaurants/restaurant_list.html'),
-		name='restaurant_list'),
+    url(r'\^\$', 
+        ListView.as_view(
+        	queryset=Restaurant.objects.filter(date__lte=timezone.now()).order_by('date')[:5],
+        	context_object_name='latest_restaurant_list',
+        	template_name='myrestaurants/restaurant_list.html'),
+        name='restaurant_list'),
 
-# Restaurant details
-	url(r'\^restaurants/(?P<pk>\d+)/\$',
-		RestaurantDetail.as_view(),
-		name='restaurant_detail'),
+# Restaurant details, ex.: /myrestaurants/restaurants/1/
+    url(r'\^restaurants/(?P<pk>\d+)/\$',
+        RestaurantDetail.as_view(),
+        name='restaurant_detail'),
 
-# Restaurant dish details
-	url(r'\^restaurants/(?P<pkr>\d+)/dishes/(?P<pk>\d+)/\$',
-		DetailView.as_view(
-			model=Dish,
-			plate_name='myrestaurants/dish_detail.html'),
-		name='dish_detail'),
+# Restaurant dish details, ex: /myrestaurants/restaurants/1/dishes/1/
+    url(r'\^restaurants/(?P<pkr>\d+)/dishes/(?P<pk>\d+)/\$',
+        DetailView.as_view(
+        	model=Dish,
+        	template_name='myrestaurants/dish_detail.html'),
+        name='dish_detail'),
 
-# Create a restaurant
-	url(r'\^restaurants/create/\$',
-		RestaurantCreate.as_view(),
-		name='restaurant_create'),
+# Create a restaurant, /myrestaurants/restaurants/create/
+    url(r'\^restaurants/create/\$',
+        RestaurantCreate.as_view(),
+        name='restaurant_create'),
 
-# Edit restaurant details
-	url(r'\^restaurants/(?P<pk>\d+)/edit/\$',
-		UpdateView.as_view(
-			model = Restaurant,
-			template_name = 'myrestaurants/form.html',
-			form_class = RestaurantForm),
-		name='restaurant_edit'),
+# Edit restaurant details, ex.: /myrestaurants/restaurants/1/edit/
+    url(r'\^restaurants/(?P<pk>\d+)/edit/\$',
+        UpdateView.as_view(
+        	model = Restaurant,
+        	template_name = 'myrestaurants/form.html',
+        	form_class = RestaurantForm),
+        name='restaurant_edit'),
 
-# Create a restaurant dish
-	url(r'\^restaurants/(?P<pk>\\d+)/dishes/create/\$',
-		DishCreate.as_view(),
-		name='dish_create'),
+# Create a restaurant dish, ex.: /myrestaurants/restaurants/1/dishes/create/
+    url(r'\^restaurants/(?P<pk>\\d+)/dishes/create/\$',
+    	DishCreate.as_view(),
+        name='dish_create'),
 
-# Edit restaurant dish details
-	url(r'\^restaurants/(?P<pkr>\\d+)/dishes/(?P<pk>\\d+)/edit/\$',
-		UpdateView.as_view(
-			model = Dish,
-			template_name = 'myrestaurants/form.html',
-			form_class = DishForm),
-		name='dish_edit'),
+# Edit restaurant dish details, ex.: /myrestaurants/restaurants/1/dishes/1/edit/
+    url(r'\^restaurants/(?P<pkr>\\d+)/dishes/(?P<pk>\\d+)/edit/\$',
+    	UpdateView.as_view(
+    		model = Dish,
+    		template_name = 'myrestaurants/form.html',
+    		form_class = DishForm),
+    	name='dish_edit'),
 
-# Create restaurant review
-	url(r'\^restaurants/(?P<pk>\\d+)/reviews/create/\$',
-		'myrestaurants.views.review',
-		name='review_create'),
-
+# Create a restaurant review, ex.: /myrestaurants/restaurants/1/reviews/create/
+# Unlike the previous patterns, this one is implemented using a method view instead of a class view
+    url(r'\^restaurants/(?P<pk>\\d+)/reviews/create/\$',
+    	review,
+    	name='review_create'),
 ]
